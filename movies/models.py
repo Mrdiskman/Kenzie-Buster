f054d5b884bb7507ac4a08b576391506c91d88fa
Mrdiskman <pedrosandes2010@gmail.com>
2022-12-16 12:23:53 -0300

    atividade corrigida

--- movies/models.py
+++ movies/models.py
@@ -14,6 +14,7 @@ class Movie(models.Model):
     rating =  models.CharField(max_length=20, choices=AgeRestriction.choices, default=AgeRestriction.G)
     synopsis = models.TextField(default=None, null=True)
     user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="movies")
+    order = models.ManyToManyField("users.User", through="movies.MovieOrder")
 
 class MovieOrder(models.Model):
     user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="buys")
